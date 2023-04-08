3c39f5abceadb1c06e53aa001162787b459c2bb3
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-08 11:44:20 -0300

    update database.js - get function

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -25,6 +25,15 @@ export const putDb = async (content) =>{
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readonly');
+  const store = tx.objectStore('todos');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
