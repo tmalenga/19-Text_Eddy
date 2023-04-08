9ab7c99a04c77c05973be443e96392d05825615f
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-08 16:55:49 -0300

    update code

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -27,9 +27,9 @@ export const putDb = async (content) =>{
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
